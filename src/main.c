8bdeccfbbc65b4f9608ca1364e1756518ab3e45c
TuurMellaerts <tuur.mellaerts1@hotmail.be>
2023-12-28 16:38:25 +0100

    Added documentation to main function

--- src/main.c
+++ src/main.c
@@ -6,6 +6,10 @@
 // General includes
 #include <stdio.h>
 
+/**
+ * @brief Main function of application. 
+ * @return -1 if failure, 0 otherwise
+*/
 int main(int argc, char *argv[])
 {
     printf("Hello World!\n");
