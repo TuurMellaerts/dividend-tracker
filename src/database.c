45858732eb6e93907345afb14c69a9ea0509cfa6
TuurMellaerts <tuur.mellaerts1@hotmail.be>
2023-12-28 16:37:25 +0100

    Started on dividendEntry function

--- src/database.c
+++ src/database.c
@@ -5,3 +5,13 @@
 
 // General includes
 #include <stdio.h>
+#include <stdbool.h>
+
+/**
+ * @brief Function to enter a dividend transaction into the database file.
+ * @return true if entry succeeded, false otherwise
+*/
+bool database_dividendEntry()
+{
+    return false;
+}
