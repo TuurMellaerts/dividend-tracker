532190600a6da52b0abe2df89d3205113f481f87
TuurMellaerts <tuur.mellaerts1@hotmail.be>
2023-12-28 16:41:49 +0100

    Added function to create database file

--- src/database.c
+++ src/database.c
@@ -8,6 +8,23 @@
 #include <stdbool.h>
 
 /**
+ * @brief Function to create the database file.
+ * @return true if creation succeeded, false otherwise
+*/
+bool database_createDatabase()
+{
+    FILE *fptr;
+
+    // Create a file
+    fptr = fopen("build/dividend-database.db", "w");
+
+    // Close the file
+    fclose(fptr);
+
+    return true;
+}
+
+/**
  * @brief Function to enter a dividend transaction into the database file.
  * @return true if entry succeeded, false otherwise
 */
